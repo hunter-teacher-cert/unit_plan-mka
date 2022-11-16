95268a9243f9fc51d1d67eb61053226e17079faf
Adam Driggers <awdriggs@gmail.com>
2022-11-16 17:55:38 -0500

    collapse 1 and 2

---
+++ lessons/04_mouse/code/mouse_lab_START.js
@@ -0,0 +1,32 @@
+var sprite1 //variable to hold Sprite
+
+function setup() {
+  createCanvas(400, 400);
+  
+  sprite1 = new Sprite(200,200,50,50) //new sprite created
+  
+}
+
+function draw() {
+  background(220);
+  
+  /////////////////  MILD  /////////////////
+  
+  //Turn the sprite RED when you mouse over it.
+  
+  
+  
+  //Turn the sprite GREEN when you click it.
+  
+  
+  
+  //Change the sprite's location by clicking and dragging it to a new location on the canvas.
+  
+  
+  
+  /////////////////  SPICY  /////////////////
+  
+  //
+  
+  
+}
