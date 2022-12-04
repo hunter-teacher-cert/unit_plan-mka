1341b15aa0d1f45cfbb7ed867c9aea93e4a8ef66
Mshuman8 <37717052+Mshuman8@users.noreply.github.com>
2022-12-04 11:18:29 -0500

    Create mouse_code_along_START.js

---
+++ lessons/04_mouse/code/mouse_code_along_START.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 400);
+  
+  
+}
+
+function draw() {
+  background(220);
+  
+ //Let's make a square sprite in the middle of the canvas.
+  
+  //When the mouse is hovering over the sprite, let's get it's color to change.
+  
+  //Let's also get it's width to increase (still when the mouse is hovering over the sprite)
+  
+  
+  
+}
